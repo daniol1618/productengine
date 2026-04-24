ea9d87114c8433dad133c45f3e4e6dfb37b99cd1
Fdalsasso <fndalsasso@gmail.com>
2026-04-24 17:41:02 -0300

    feat: OrderDTO (why not?)

---
+++ src/main/java/com/api/productengine/dto/OrderDTO.java
@@ -0,0 +1,16 @@
+package com.api.productengine.dto;
+
+import java.math.BigDecimal;
+
+public class OrderDTO {
+    private Long productId;
+    private BigDecimal price;
+
+    public getProductId() {
+        return productId;
+    }
+
+    public getPrice() {
+        return price;
+    }
+}
