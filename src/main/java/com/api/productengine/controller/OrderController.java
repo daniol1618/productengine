81a66bff697154dcdfe4f8bf5f43faf4b1ba6bc4
Gonzalo Ivo Turri <gturri@frba.utn.edu.ar>
2026-04-24 17:36:12 -0300

    chore: order controller and service

---
+++ src/main/java/com/api/productengine/controller/OrderController.java
@@ -0,0 +1,34 @@
+package com.api.productengine.controller;
+
+import com.api.productengine.dto.order.OrderCreateRequestDTO;
+import com.api.productengine.dto.order.OrderResponseDTO;
+import com.api.productengine.service.OrderService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public OrderResponseDTO create(@RequestBody OrderCreateRequestDTO createRequest) {
+        return this.orderService.createOrder(createRequest);
+    }
+
+    @GetMapping
+    public List<OrderResponseDTO> getAll(){
+        return this.orderService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public OrderResponseDTO findById(@PathVariable("id") Long id){
+        return this.orderService.findById(id);
+    }
+}
