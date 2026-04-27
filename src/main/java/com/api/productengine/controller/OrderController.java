4ab6ed41d87dee3ee74a9a504116a2db21133a37
Giovanni Cervantes <giovacervantes08@gmail.com>
2026-04-27 13:34:07 -0600

    Commit Reversible para hacer pull request y seguir con la otra act

---
+++ src/main/java/com/api/productengine/controller/OrderController.java
@@ -0,0 +1,47 @@
+package com.api.productEngine.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.api.productengine.model.Order;
+import com.api.productengine.service.OrderService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    private final OrderService service;
+
+    public OrderController(OrderService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Order create(@RequestBody Order order) {
+        return service.create(order);
+    }
+
+    @GetMapping
+    public List<Order> getAll() {
+        System.out.println("exposed dta");
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Order getById(@PathVariable Long id) {
+        return service.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Order update(@PathVariable Long id, @RequestBody Order order) {
+        return service.update(id, order);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+}
---
+++ src/main/java/com/api/productengine/model/Order.java
@@ -0,0 +1,46 @@
+package com.api.productengine.model;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+import org.antlr.v4.runtime.misc.NotNull;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
