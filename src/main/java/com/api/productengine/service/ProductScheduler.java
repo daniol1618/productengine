6242d67b1cfa644f551a34dd1dcf825ff3898cb2
Daniel Olarte <daniol1618@gmail.com>
2026-04-28 14:17:57 -0500

    feat: scheduler sample

--- src/main/java/com/api/productengine/service/ProductScheduler.java
+++ src/main/java/com/api/productengine/service/ProductScheduler.java
@@ -24,17 +24,18 @@ public class ProductScheduler {
     /**
      * @Scheduled enables the method to run periodically based on the configuration.
      * The 'cron' attribute defines the execution schedule using a cron expression.
-     * '${app.scheduling.product-save-cron}' is a placeholder that retrieves the 
+     * '${app.scheduling.product-save-cron}' is a placeholder that retrieves the
      * actual cron expression from the application.yaml file.
      */
     @Scheduled(cron = "${app.scheduling.product-save-cron}")
     public void saveNewProduct() {
         Product product = new Product();
-        product.setName("Auto Product " + LocalDateTime.now());
+        product.setName("My Batch Product " + LocalDateTime.now());
         product.setDescription("Automatically generated product at " + LocalDateTime.now());
         product.setPrice(new BigDecimal("19.99"));
         product.setStock(100);
 
+        System.out.println("The product to be saved is" + product.getName());
         productRepository.save(product);
         System.out.println("Saved new product: " + product.getName());
     }
